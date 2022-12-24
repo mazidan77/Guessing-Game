7efc583e4e7bee821b171400f5278cfd00646a8c
mohamed zidan <moga4arab@gmail.com>
2022-12-24 20:56:58 +0200

    Add files via upload

---
+++ js/words.js
@@ -0,0 +1,32 @@
+let wordList = [
+
+    {
+        word: "react",
+        hint: "imgs/react.png"
+    },
+    {
+        word: "iti",
+        hint: "imgs/iti.png"
+    },
+    {
+        word: "javascript",
+        hint: "imgs/javascript.png"
+    },
+    {
+        word: "css",
+        hint: "imgs/css.svg"
+    },
+    {
+        word: "angular",
+        hint: "imgs/Angular.png"
+    },
+    {
+        word: "github",
+        hint: "imgs/git.svg"
+    },
+    {
+        word: "python",
+        hint: "imgs/python.png"
+    },
+   
+]
